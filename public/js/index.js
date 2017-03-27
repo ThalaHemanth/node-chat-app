1881ebfa1ead683340a6f1f1f51896ca64fbca00
ThalaHemanth <viruhemanth@gmail.com>
2017-03-27 23:41:43 +0530

    added css

--- public/js/index.js
+++ public/js/index.js
@@ -17,14 +17,43 @@
     $('#messages').append(li);
   })
 
+  socket.on('newLocationMessage', function(message) {
+    var li = $('<li></li>');
+    var a = $('<a target=_blank>My Current Location </a>');
+    li.text(`${message.from}:`);
+    a.attr(`href`,message.url);
+    li.append(a);
+    $('#messages').append(li);
+  });
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
-
+  var messageTextbox = $('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
+      messageTextbox.val('')
+  });
+});
 
+var locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+
+  if(!navigator.geolocation) {
+    return alert('Geolocation Not Supported');
+  }
+
+  locationButton.attr('disabled','disabled').text('Sending Location');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send Location');
+    socket.emit('createLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }, function() {
+    locationButton.removeAttr('disabled').text('Send Location');
   });
 });
