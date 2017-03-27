c0fe09de8211a3d4e248a083a3fa62f228bbdc91
ThalaHemanth <viruhemanth@gmail.com>
2017-03-27 07:19:18 +0530

    create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,13 @@
-  const socket = io();
 
+  const socket = io();
   socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-      from: 'Finch',
-      text: 'India is Incredible'
-    });
   });
-
-    socket.on('newMessage', function(message) {
-    console.log('New Message',message);
-  });
-
 
   socket.on('disconnect', function () {
     console.log('Disconnected from the server');
   });
+
+  socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+  })
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User Connected');
 
-  socket.emit('newMessage', {
-    from: 'Hem',
-    message: 'Hello john',
-    createdAt: new Date()
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage',message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
@@ -31,6 +30,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(port,  () => {
+server.listen(port, () => {
   console.log(`Express Server Running on ${port}`);
 });
