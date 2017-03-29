0e88a61e02d059c4a6a89168799c72a68b49062a
ThalaHemanth <viruhemanth@gmail.com>
2017-03-29 17:09:40 +0530

    user list and chat rooms

--- server/server.js
+++ server/server.js
@@ -5,20 +5,40 @@ const socketIO = require('socket.io');
 
 const { generateMessage, generateLocationMessage } = require('./utils/message');
 
+const { isRealString } = require('./utils/validation');
+
+const { Users } = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New User Connected');
-  socket.emit('newMessage', generateMessage('Admin','Welcome to chat Room'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin','new User Connected'))
+
+  socket.on('join',(params,callback) => {
+      if(!isRealString(params.name) || !isRealString(params.room)) {
+        callback('Name and room name are required');
+      }
+      socket.join(params.room);
+      users.removeUser(socket.id);
+      users.addUser(socket.id,params.name,params.room);
+
+      io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+      socket.emit('newMessage', generateMessage('Admin','Welcome to chat Room'));
+
+      socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',`${params.name} has  Joined`));
+      callback();
+  });
+
   socket.on('createMessage', (message,callback) => {
     console.log('createMessage',message);
     io.emit('newMessage', generateMessage(message.from,message.text));
@@ -30,7 +50,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log("Client Disconnected");
+    var user = users.removeUser(socket.id);
+    if(user) {
+      io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+      io.to().emit('newMessage',generateMessage('Admin',`${user.name} left`));
+    }
   });
 });
 
