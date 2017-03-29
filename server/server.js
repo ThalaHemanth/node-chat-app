21bfe198f9c4ea4729459430c779ef3d2b092fed
ThalaHemanth <viruhemanth@gmail.com>
2017-03-29 20:11:41 +0530

    user list and chat rooms

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message,callback) => {
     var user = users.getUser(socket.id);
     if(user && isRealString(message.text)) {
-      io.to(user.room).emit('newMessage', generateMessage(user.room,message.text));
+      io.to(user.room).emit('newMessage', generateMessage(user.name,message.text));
     }
     callback();
   });
